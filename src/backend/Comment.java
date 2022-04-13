ccce545003ff706ad7f995c619c2f38464e81f60
BGermroth <ipreferthisforbusiness@gmail.com>
2022-04-13 17:38:26 -0400

    Cleaned Repository

---
+++ src/backend/Comment.java
@@ -0,0 +1,46 @@
+package backend;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+/**
+ * @author Pujit
+ * 
+ * @version p 0.1 (p is for prototype, v is for version)
+ * 
+ * Extends backend.HistoryItem
+ * Represents Comment on a task
+ *
+ */
+public class Comment extends HistoryItem implements Serializable {
+	private String comment;
+	
+	public Comment(String userComment)
+	{
+		comment = userComment;
+		setDate(Calendar.getInstance().getTime());//Is this the correct way to set the date?
+	}
+	
+	/** @author Pujit
+	 * @return comment: Returns comment the object represent
+	 */
+	public String getComment() 
+	{
+		return comment;
+	}
+	
+	/**
+	 * @author Pujit
+	 * @param newComment: The new comment that replaces the old one
+	 */
+	public void setComment(String newComment)
+	{
+		comment = newComment;
+	}
+	
+	public boolean isEqual(Comment comment) {
+		if(comment.getComment().equals(this.getComment()))
+			return true;
+		return false;
+	}
+}
---
+++ src/backend/HistoryItem.java
@@ -0,0 +1,26 @@
+package backend;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public abstract class HistoryItem implements Serializable{
+	private Date whenDone;
+	private String text = "";
+	
+	public Date getDate() {
+		return whenDone;
+	}
+	
+	public void setDate (Date date) {
+		whenDone = date;
+	}
+
+	public void setText(String eventDescription){
+		text = eventDescription;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+}
